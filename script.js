1df009832b56f7d7106f99235f0fba8893961fb2
CaioWellisson <74879462+pudim-cell@users.noreply.github.com>
2026-05-11 11:45:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -63,3 +63,65 @@ function calcularIMC(){
     resultado.innerText =
     `IMC: ${imc.toFixed(2)}`;
 }
+
+function gerarDieta(){
+
+    const objetivo =
+    document.getElementById('objetivo').value;
+
+    const resultado =
+    document.getElementById('resultadoDieta');
+
+    let dieta = '';
+
+    if(objetivo === 'emagrecer'){
+
+        dieta = `
+        🍳 Café da manhã:
+        Omelete + café sem açúcar
+
+        🥗 Almoço:
+        Frango grelhado + arroz integral + salada
+
+        🍎 Lanche:
+        Frutas e castanhas
+
+        🥩 Jantar:
+        Peixe + legumes
+        `;
+    }
+
+    else if(objetivo === 'ganhar'){
+
+        dieta = `
+        🍳 Café da manhã:
+        Ovos + pão integral + vitamina
+
+        🍗 Almoço:
+        Frango + arroz + feijão + batata doce
+
+        🥤 Pós treino:
+        Whey protein + banana
+
+        🥩 Jantar:
+        Carne vermelha + macarrão integral
+        `;
+    }
+
+    else{
+
+        dieta = `
+        🥗 Dieta equilibrada:
+
+        Café da manhã saudável
+
+        Proteína no almoço
+
+        Frutas nos lanches
+
+        Jantar leve
+        `;
+    }
+
+    resultado.innerText = dieta;
+}
